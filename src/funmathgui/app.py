a06f27525da6826b05be71b807032349b3fb6449
stround124 <stround.lunar@hotmail.com>
2026-03-06 22:32:16 +0200

    Update app.py

--- src/funmathgui/app.py
+++ src/funmathgui/app.py
@@ -57,7 +57,6 @@ async def checkAnswer(widget):
     try:
         user_input = widget.value.strip()
 
-        # Ако има дроб (пример 1/2)
         if "/" in user_input:
             user_value = float(fractions.Fraction(user_input))
         else:
@@ -75,7 +74,7 @@ async def checkAnswer(widget):
 
     except:
         await widget.app.main_window.dialog(
-            toga.InfoDialog("Грешка", "Моля въведи число или дроб (пример: 1/2)")
+            toga.InfoDialog("Грешка", "Моля въведи число или дроб")
         )
         return
 
@@ -136,7 +135,7 @@ def askMultiplication(app):
     num1 = random.randint(1, j-1)
     num2 = random.randint(1, j-1)
     app.resTrue = num1 * num2
-    return f"Пресметни {num1} x {num2}"
+    return f"Пресметни {num1} * {num2}"
 
 def askDivision(app):
     match app.level:
@@ -207,30 +206,14 @@ class FunMathGUI(toga.App):
         random.seed()
         self.answer_img = toga.Image(self.paths.app / "resources/main_menu.jpg")
         self.answer_imgview = toga.ImageView(self.answer_img, height = 240, flex = 1, margin = 10)
-        # self.canvas_logo = toga.Canvas(
-        #     flex=1,
-        #     on_resize=self.on_resize,
-        #     on_press=self.on_press,            
-        # )
-        # self.logo_view = toga.Box(direction=COLUMN, margin=20)
         self.main_view = toga.Box(direction=COLUMN, alignment=CENTER, margin=50)
 
-        # self.draw_logo()
         self.main_menu()
 
-        # container_left = self.logo_view
-        # container_right = toga.ScrollContainer(horizontal=False)
-        # container_left.add(self.canvas_logo)
-        # container_left.add(self.bg_imgview)
-        # container_right.content = self.main_view
-
-        # split_main = toga.SplitContainer()
-        # split_main.content = [(container_left, 1), (container_right, 2)]
 
         self.main_window = toga.Window(title=self.formal_name)
         self.main_window.content = self.main_view
         self.main_window.show()
-        # self.enter_presentation_mode([self.main_window]) # Fullscreen mode
 
     
     def main_menu(self):
@@ -259,46 +242,13 @@ class FunMathGUI(toga.App):
         curr_menu.add(btnExit)
         btnAdd.focus()
 
-    # def draw_logo(self):
-        
-    #     font = toga.Font(family=SANS_SERIF, size=20)
-    #     self.text_width, text_height = self.canvas_logo.measure_text("Fun Math GUI", font)
-
-    #     x = (150 - self.text_width) // 2
-    #     y = 10
-
-    #     with self.canvas_logo.Stroke(color="CYAN", line_width=4.0) as rect_stroker:
-    #         self.text_border = rect_stroker.rect(
-    #             x - 5,
-    #             y - 5,
-    #             self.text_width + 10,
-    #             text_height + 10,
-    #         )
-    #     with self.canvas_logo.Fill(color="LIGHTGREEN") as text_filler:
-    #         self.text = text_filler.write_text("Fun Math GUI", x, y, font, Baseline.TOP)
-
-    # def on_resize(self, widget, width, height, **kwargs):
-    #     # On resize, center the text horizontally on the canvas. on_resize will be
-    #     # called when the canvas is initially created, when the drawing objects won't
-    #     # exist yet. Only attempt to reposition the text if there's context objects on
-    #     # the canvas.
-    #     if widget.context:
-    #         left_pad = (width - self.text_width) // 2
-    #         self.text.x = left_pad
-    #         self.text_border.x = left_pad - 5
-    #         widget.redraw()
-
-    # async def on_press(self, widget, x, y, **kwargs):
-    #     await self.main_window.dialog(
-    #         toga.InfoDialog("Хмм?", "Ти защо цъкаш тук?")
-    #     )
     
     async def on_exit(self):
         self.main_view.clear()
         self.answer_img = toga.Image(self.paths.app / "resources/sad.jpg")        
         self.answer_imgview.image = self.answer_img
         self.main_view.add(self.answer_imgview)
-        lblBye = toga.Label(text="Не трябваше да натискаш този бутон...", margin=20)
+        lblBye = toga.Label(text="Не трябваше да натискаш този бутон :(", margin=20)
         self.main_view.add(lblBye)
         await asyncio.sleep(3)
         return True
